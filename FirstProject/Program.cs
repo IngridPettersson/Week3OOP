4cc34d90cfa391393da2424eb65cd6376b319b9f
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-02-01 09:49:20 +0100

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        // klassvariabel = fält = field/public field
+        // instans = object
+        // referenstyp = reference type ( och i kontexten av C# även = pekare = pointer)
+
+        const string name = "Strumpan";
+        static void Main(string[] args)
+        {
+            const string lillansName = "Madame";
+            Console.WriteLine(name);
+            Console.WriteLine(lillansName);
+            
+        }
+    }
+}
---
+++ enums/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace enums
+{
+    class Program
+    {
+
+        // Kan skapa egna enums såhär:
+        enum WeekDay
+        {
+            // Databaser (i af SQL) förstår inte enum så till den behöver skicka in intar (t. ex?)
+            Monday = 10, Tuesday = 20
+        }
+        static void Main(string[] args)
+        {
+            AddCalendarEvent("Do something", DayOfWeek.Monday, WeekDay.Monday);
+        }
+
+        static void AddCalendarEvent(string eventName, DayOfWeek dayOfWeek, WeekDay weekDay)
+        {
+            // Genom att skriva switch tab tab och sedan lägger in dayOfWeek i parentesen och trycker Enter Enter så printas hela färdiga switch statement ut enligt nedan:
+            switch (dayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    break;
+                case DayOfWeek.Monday:
+                    Console.WriteLine((int)dayOfWeek); // 1
+                    Console.WriteLine((int)weekDay); // 10 (se ovan)
+                    break;
+                case DayOfWeek.Tuesday:
+                    break;
+                case DayOfWeek.Wednesday:
+                    break;
+                case DayOfWeek.Thursday:
+                    break;
+                case DayOfWeek.Friday:
+                    break;
+                case DayOfWeek.Saturday:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
